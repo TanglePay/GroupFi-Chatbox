2031c7f7e9d12976d73cb522881775c2d6787ea8
郭维 <guodawei1021@gmail.com>
2023-10-31 17:28:31 +0800

    feat(route)

--- packages/pc/src/App.tsx
+++ packages/pc/src/App.tsx
@@ -1,4 +1,8 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  BrowserRouter
+} from 'react-router-dom'
 import { AppWrapper, Loading } from 'components/Shared'
 import { useEffect } from 'react'
 import { MqttClient } from '@iota/mqtt.js'
@@ -15,38 +19,68 @@ const routes = [
     url: './components/GroupList'
   },
   {
-    path: '/group/:id',
+    path: 'group/:id',
     url: './components/ChatRoom'
   },
   {
-    path: '/group/:id/info',
+    path: 'group/:id/info',
     url: './components/GroupInfo'
   },
   {
-    path: '/user/:id',
+    path: 'user/:id',
     url: './components/UserInfo'
   }
 ]
 
-const router = createBrowserRouter(
-  routes.map(({ path, url }) => {
-    return {
-      path,
-      async lazy() {
-        const Component = (await import(url)).default
-        return { Component }
-      }
+// const router = createBrowserRouter(
+//   routes.map(({ path, url }) => {
+//     return {
+//       path,
+//       async lazy() {
+//         const Component = (await import(url)).default
+//         return { Component }
+//       }
+//     }
+//   })
+// )
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    async lazy() {
+      const Component = (await import('./components/GroupList')).default
+      return { Component }
     }
-  })
-)
+  },
+  {
+    path: 'group/:id',
+    async lazy() {
+      const Component = (await import('./components/ChatRoom')).default
+      return { Component }
+    }
+  },
+  {
+    path: 'group/:id/info',
+    async lazy() {
+      const Component = (await import('./components/GroupInfo')).default
+      return { Component }
+    }
+  },
+  {
+    path: 'user/:id',
+    async lazy() {
+      const Component = (await import('./components/UserInfo')).default
+      return { Component }
+    }
+  }
+])
 
 function App() {
   const { messageDomain } = useMessageDomain()
   const groupFiService = useGroupFiService()
   const fn = async () => {
     await messageDomain.connectWallet()
-    const mqttWrapper = window as unknown as { mqtt: any }
-    await messageDomain.setupGroupFiMqttConnection(mqttWrapper.mqtt.connect)
+    await messageDomain.setupGroupFiMqttConnection(connect)
     await groupFiService.setupIotaMqttConnection(MqttClient)
     const adapter = new LocalStorageAdaptor()
     messageDomain.setStorageAdaptor(adapter)
