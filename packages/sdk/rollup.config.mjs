1276d4ef2c2073f78d5f761d64853d16fa95852c
alex yuan <alexmengyuan2022@gmail.com>
2023-11-22 20:27:52 +0800

    fix build error

--- packages/sdk/rollup.config.mjs
+++ packages/sdk/rollup.config.mjs
@@ -1,5 +1,5 @@
 import {createRollupConfig, decorateIifeExternal} from "./rollup.base.mjs";
 import pkg from './package.json' assert { type: "json" }
 const config =  createRollupConfig(pkg)
-
+decorateIifeExternal(config[0],{tslib: 'tslib'})
 export default config
