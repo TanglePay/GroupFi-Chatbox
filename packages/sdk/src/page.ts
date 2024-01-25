212f84295d3d34bbd0c799fe2f6774a4706a8091
郭维 <guodawei1021@gmail.com>
2024-01-25 20:23:29 +0800

    groupfi sdk using prerelease trollbox

--- packages/sdk/src/page.ts
+++ packages/sdk/src/page.ts
@@ -100,7 +100,7 @@ export const genOnLoad = (init: (context: TargetContext) => void) => () => {
     });
   };
 
-  iframe.src = 'https://test.trollbox.iotacat.com';
+  iframe.src = 'https://prerelease.trollbox.groupfi.ai';
 
   setStyleProperties.bind(iframe.style)({
     width: '100%',
