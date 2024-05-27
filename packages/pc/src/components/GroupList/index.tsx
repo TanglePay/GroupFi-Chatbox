728179c78653153bbcaf9d76e9db0fd79b4ad49e
time-endless <138768192+time-endless@users.noreply.github.com>
2024-05-27 14:33:19 +0800

    hide myGroups Tab (#592)

--- packages/pc/src/components/GroupList/index.tsx
+++ packages/pc/src/components/GroupList/index.tsx
@@ -211,7 +211,7 @@ function NoGroupPrompt(props: { groupType: 'mygroup' | 'forme' }) {
     groupType === 'forme'
       ? 'No Available Group For You'
       : groupType === 'mygroup'
-      ? "You haven't Joined in / Subscribed Any Groups"
+      ? "You don't have any groups yet"
       : ''
   return (
     <div className={classNames('mt-[132px]')}>
