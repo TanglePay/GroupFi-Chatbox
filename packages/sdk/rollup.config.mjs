5d881f2e8ea21ee7c1ab2667436ac50e16ef668d
alex yuan <alexmengyuan2022@gmail.com>
2024-07-05 23:12:27 +0800

    fix build

--- packages/sdk/rollup.config.mjs
+++ packages/sdk/rollup.config.mjs
@@ -1,5 +1,38 @@
-import {createRollupConfig, decorateIifeExternal} from "../../rollup.base.mjs";
+import image from '@rollup/plugin-image';
+import postcss from 'rollup-plugin-postcss'
+import url from 'postcss-url'
+import {createRollupConfig, decorateIifeExternal,decoratePlugin} from "../../rollup.base.mjs";
 import pkg from './package.json' assert { type: "json" }
 const config =  createRollupConfig(pkg)
-decorateIifeExternal(config[0],{})
+/*
+image({
+                extensions: ['.png', '.jpg', '.jpeg', '.gif', '.svg']
+            }),
+            postcss({
+               extract: 'assets/style.css',
+               minimize: true,
+               plugins: [
+                url({
+                    url: 'inline'
+                })
+               ]
+            })*/
+decoratePlugin(config,image({
+    extensions: ['.png', '.jpg', '.jpeg', '.gif', '.svg']
+}))
+decoratePlugin(config,postcss({
+    extract: 'assets/style.css',
+    minimize: true,
+    plugins: [
+     url({
+         url: 'inline'
+     })
+    ]
+ }))
+ decorateIifeExternal(config[0],{
+    '@iota/iota.js': 'Iota',
+    '@iota/crypto.js':'IotaCrypto',
+    '@iota/util.js': 'IotaUtil',
+    'big-integer':'bigInt',
+})
 export default config
--- packages/shared/rollup.config.mjs
+++ packages/shared/rollup.config.mjs
@@ -1,5 +1,10 @@
 import {createRollupConfig, decorateIifeExternal} from "../../rollup.base.mjs";
 import pkg from './package.json' assert { type: "json" }
 const config =  createRollupConfig(pkg)
-
+decorateIifeExternal(config[0],{
+    '@iota/iota.js': 'Iota',
+    '@iota/crypto.js':'IotaCrypto',
+    '@iota/util.js': 'IotaUtil',
+    'big-integer':'bigInt',
+})
 export default config
