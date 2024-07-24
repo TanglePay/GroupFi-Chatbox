d7b3de1401a2ad7d3f5b0cf27e80214ea49e90f6
time-endless <138768192+time-endless@users.noreply.github.com>
2024-07-24 15:51:26 +0800

    feat: solana support (#758)

--- packages/sdk/src/handleRequest.ts
+++ packages/sdk/src/handleRequest.ts
@@ -1,8 +1,13 @@
-import TrollboxSDK  from "./index";
+import TrollboxSDK from './index'
+import { hexStringToUint8Array, uint8ArrayToHexString } from './util'
+
+const isEthereumProvider = (provider: any) => {
+  return provider.chainId !== undefined
+}
 
 export const requestHandler = {
   async handle(method: string, params: any) {
-    switch(method) {
+    switch (method) {
       case 'eth_decrypt':
         return await this.ethDecrypt(params)
       case 'personal_sign':
@@ -22,11 +27,11 @@ export const requestHandler = {
       // });
       const res = await TrollboxSDK.walletProvider.request({
         method: 'eth_decrypt',
-        params: params,
-      });
-      return { code: 200, res };
+        params: params
+      })
+      return { code: 200, res }
     } catch (error) {
-      return { code: 9999 };
+      return { code: 9999 }
     }
   },
   personalSign: async (params: any) => {
@@ -38,13 +43,24 @@ export const requestHandler = {
       //   method: 'personal_sign',
       //   params,
       // });
-      const res = await TrollboxSDK.walletProvider.request({
-        method: 'personal_sign',
-        params,
-      });
-      return { code: 200, res };
+      let res: any
+      const [signTextHex, address] = params
+      if (isEthereumProvider(TrollboxSDK.walletProvider)) {
+        res = await TrollboxSDK.walletProvider.request({
+          method: 'personal_sign',
+          params: [signTextHex, address]
+        })
+      } else {
+        const encodedMessage = hexStringToUint8Array(signTextHex)
+        const signedMessage = await TrollboxSDK.walletProvider.signMessage(
+          encodedMessage,
+          'utf8'
+        )
+        res = uint8ArrayToHexString(signedMessage.signature)
+      }
+      return { code: 200, res }
     } catch (error) {
-      return { code: 9999 };
+      return { code: 9999 }
     }
   },
   ethGetEncryptionPublicKey: async (params: any) => {
@@ -58,11 +74,11 @@ export const requestHandler = {
       // })) as string;
       const res = (await TrollboxSDK.walletProvider.request({
         method: 'eth_getEncryptionPublicKey',
-        params,
-      })) as string;
-      return { code: 200, res };
+        params
+      })) as string
+      return { code: 200, res }
     } catch (error) {
-      return { code: 9999 };
+      return { code: 9999 }
     }
-  },
-};
+  }
+}
