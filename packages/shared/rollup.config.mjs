e10f036b9898ebc770ff0043c39052982b0fb303
alex yuan <alexmengyuan2022@gmail.com>
2024-08-01 16:31:32 +0800

    keep classname for logging

--- packages/shared/rollup.config.mjs
+++ packages/shared/rollup.config.mjs
@@ -1,6 +1,6 @@
 import {createRollupConfig, decorateIifeExternal} from "../../rollup.base.mjs";
 import pkg from './package.json' assert { type: "json" }
-const config =  createRollupConfig(pkg)
+const config =  createRollupConfig(pkg,{keepClassNames:true})
 decorateIifeExternal(config[0],{
     '@iota/iota.js': 'Iota',
     '@iota/crypto.js':'IotaCrypto',
