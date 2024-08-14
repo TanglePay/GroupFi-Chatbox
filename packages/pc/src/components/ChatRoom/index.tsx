ec71bcc4358b4f669180f3437575c8c4a3353e90
郭维 <guodawei1021@gmail.com>
2024-08-14 13:13:03 +0800

    improve: style

--- packages/pc/src/components/ChatRoom/index.tsx
+++ packages/pc/src/components/ChatRoom/index.tsx
@@ -698,7 +698,7 @@ function MarkedContent(props: {
           'font-medium mx-1 inline-block truncate align-bottom'
         )}
         style={{
-          maxWidth: `calc(100% - 120px)`
+          maxWidth: `calc(100% - 140px)`
         }}
       >
         {qualifyType === 'nft'
