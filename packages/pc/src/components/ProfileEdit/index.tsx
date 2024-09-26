275adb4e400f391553b389388a5da069e16c19f7
time-endless <138768192+time-endless@users.noreply.github.com>
2024-09-26 18:47:56 +0800

    improve: styles (#886)

--- packages/pc/src/components/ProfileEdit/index.tsx
+++ packages/pc/src/components/ProfileEdit/index.tsx
@@ -1,13 +1,18 @@
 import useProfile from 'hooks/useProfile'
 import { UserNameCreation } from '../UserName'
 
+import { useNavigate } from 'react-router-dom'
+
 function ProfileEdit() {
   const profile = useProfile()
+  const navigate = useNavigate()
+
   return (
     <UserNameCreation
       onMintFinish={() => {}}
       currentProfile={profile}
       hasReturnIcon={true}
+      navigateFunc={navigate}
     />
   )
 }
