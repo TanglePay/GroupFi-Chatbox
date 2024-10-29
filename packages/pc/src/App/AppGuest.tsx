8c875b1fad2c9f35811d43756a63d5dd9fd24bbe
郭维 <guodawei1021@gmail.com>
2024-10-29 16:28:04 +0800

    feat: remove unused code

--- packages/pc/src/App/AppGuest.tsx
+++
@@ -1,5 +0,0 @@
-import { renderCeckRenderWithDefaultWrapper } from 'components/Shared'
-
-export default function AppGuest() {
-  return renderCeckRenderWithDefaultWrapper(<div>You are a guest</div>)
-}
