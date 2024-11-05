86f433e48082fb8f575673832fa48ff57cd68ca5
郭维 <guodawei1021@gmail.com>
2024-11-05 14:35:40 +0800

    use prerelease.chatbox.groupfi.ai

--- packages/sdk/src/page.ts
+++ packages/sdk/src/page.ts
@@ -207,8 +207,7 @@ function generateIframeSrc(params: RenderChatboxOptions) {
     searchParams.append('isBrowseMode', 'true')
   }
 
-  // return `http://localhost:5173?${searchParams.toString()}`
-  return `https://test.chatbox.groupfi.ai?${searchParams.toString()}`
+  return `https://prerelease.chatbox.groupfi.ai?${searchParams.toString()}`
 }
 
 function generateBackdropDOM() {
