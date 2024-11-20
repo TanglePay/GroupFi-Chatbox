840ffee730c5ca418a10185c3b73acbbe680b7f1
time-endless <138768192+time-endless@users.noreply.github.com>
2024-11-20 12:17:34 +0800

    test (#994)

--- packages/sdk/src/page.ts
+++ packages/sdk/src/page.ts
@@ -207,8 +207,8 @@ function generateIframeSrc(params: RenderChatboxOptions) {
     searchParams.append('isBrowseMode', 'true')
   }
 
-  // return `http://localhost:5173?${searchParams.toString()}`
-  return `https://test.chatbox.groupfi.ai?${searchParams.toString()}`
+  return `http://localhost:5173?${searchParams.toString()}`
+  // return `https://test.chatbox.groupfi.ai?${searchParams.toString()}`
 }
 
 function generateBackdropDOM() {
