2968f9d91dee820baa2e9d4725bcd5e4c058a24b
time-endless <138768192+time-endless@users.noreply.github.com>
2024-11-20 09:48:15 +0800

    test (#989)

--- packages/pc/src/components/ChatRoom/VirtualList.tsx
+++ packages/pc/src/components/ChatRoom/VirtualList.tsx
@@ -164,27 +164,27 @@ export function RowVirtualizerDynamic(props: {
 
       handler(element.getBoundingClientRect())
 
-      const observer = new ResizeObserver((entries) => {
-        console.log('===>test Enter observer')
-        const entry = entries[0]
-        console.log('===>test Enter observer entry', entry?.borderBoxSize)
-        console.log('===>test getBoundingClientRect', element.getBoundingClientRect())
-        if (entry?.borderBoxSize) {
-          const box = entry.borderBoxSize[0]
-          if (box.inlineSize > 0 && box.blockSize > 0) {
-            handler({ width: box.inlineSize, height: box.blockSize })
+      // const observer = new ResizeObserver((entries) => {
+      //   console.log('===>test Enter observer')
+      //   const entry = entries[0]
+      //   console.log('===>test Enter observer entry', entry?.borderBoxSize)
+      //   console.log('===>test getBoundingClientRect', element.getBoundingClientRect())
+      //   if (entry?.borderBoxSize) {
+      //     const box = entry.borderBoxSize[0]
+      //     if (box.inlineSize > 0 && box.blockSize > 0) {
+      //       handler({ width: box.inlineSize, height: box.blockSize })
 
-            return
-          }
-        }
-        handler(element.getBoundingClientRect())
-      })
+      //       return
+      //     }
+      //   }
+      //   handler(element.getBoundingClientRect())
+      // })
 
-      observer.observe(element, { box: 'border-box' })
+      // observer.observe(element, { box: 'border-box' })
 
-      return () => {
-        observer.unobserve(element)
-      }
+      // return () => {
+      //   observer.unobserve(element)
+      // }
     },
     getItemKey: (index: number) => {
       const messageItem = messageList[messageList.length - 1 - index]
