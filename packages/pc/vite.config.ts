1cec47b3c0c8678cdc8a7981abda9596a8c1f428
time-endless <138768192+time-endless@users.noreply.github.com>
2024-12-23 17:35:10 +0800

    perf: optimize font file (#1027)

--- packages/pc/vite.config.ts
+++ packages/pc/vite.config.ts
@@ -1,15 +1,51 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import svgr from 'vite-plugin-svgr'
 import createCompressPlugin from 'vite-plugin-compression'
 
+function preloadMainJsPlugin(): Plugin {
+  return {
+    name: 'preload-main-js',
+    transformIndexHtml(html, ctx) {
+      if (!ctx.bundle) return html
+
+      const entryChunk = Object.values(ctx.bundle).find(
+        (chunk: any) => chunk.isEntry
+      )
+
+      if (!entryChunk) return html
+
+      // 添加预加载标签
+      return {
+        html,
+        tags: [
+          {
+            tag: 'link',
+            attrs: {
+              rel: 'modulepreload',
+              href: `/${entryChunk.fileName}`,
+              crossorigin: '',
+            },
+            injectTo: 'head-prepend',
+          }
+        ]
+      }
+    }
+  }
+}
+
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   build: {
     emptyOutDir: false, // Do not clear the `dist` folder before building
     minify: 'terser', // 使用 terser 进行代码压缩
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   plugins: [
     react(),
@@ -26,7 +62,8 @@ export default defineConfig({
     }),
     createCompressPlugin({
       algorithm: 'gzip'
-    })
+    }),
+    preloadMainJsPlugin()
   ],
   // test: {
   //   globals: true,
