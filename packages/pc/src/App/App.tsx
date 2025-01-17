aeb2b59f2d0a5478d26d0ee4faf7b48cd06349dc
alex yuan <alexmengyuan2022@gmail.com>
2025-01-17 16:17:49 +0800

    adjust one group navigating of forme to consider setGroups as well

--- packages/pc/src/App/App.tsx
+++ packages/pc/src/App/App.tsx
@@ -178,18 +178,14 @@ function useHandleChangeRecommendChatGroup() {
       if (chatGroups.length === 1) {
         const groupId = removeHexPrefixIfExist(chatGroups[0].groupId)
         await router.navigate(`/group/${groupId}?home=true`)
-      } else if (chatGroups.length > 1) {
-        await router.navigate('/')
       }
     }
     setIsFirstFinished(true)
   }
 
   useEffect(() => {
-    // Sometimes, for example, when you need to log in, the chat request is already completed.
-    // so exec navigateToChatRoom at once
     navigateToChatRoom()
-  }, [])
+  }, [isForMeGroupsLoading])
 
   // Listen for changes to setGroups.
   useEffect(() => {
